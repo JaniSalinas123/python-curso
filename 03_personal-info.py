999f8ca7ebb597ee1959cac46cbd9f3e105a938e
Janise Salinas <janibrachel@gmail.com>
2023-09-11 21:37:51 -0500

    ok

---
+++ 03_personal-info.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 11 20:13:35 2023
+
+@author: Janise
+"""
+
+nombre=input("Ingrese su nombre:")
+apellido=input("Ingrese su apellido:")
+carrera=input("ingrese su carrera:")
+semestre=int(input("ingrese la cantidad de semestres que estudio: "))
+space= " "
+print(nombre+space+apellido+space+"estudio"+space+carrera+space+"durante"+space+str(semestre))
---
+++ holamundospyder.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("Hello World")
+type(98.08)
+a = 5
+b = 9
+print(a/b)
+resultado = a*b
+print(resultado)
+str1 = ("cisco")
+str2 = ("academy")
+print(str1, " ", str2)
+
+x = 5
+print(type(x))
+print("el numero de x es:" + str(x))
+print(type(x))
+
+x = str(x)nprint(type(x))c
+
+lista=[1,2,'hola']
+print((lista))
